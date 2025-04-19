d01e213312ac3875029b8ea6f960e83912d08320
AkshayaSelvam4 <akshayaselvam4@gmail.com>
2025-04-19 16:27:17 +0530

    created connection class

---
+++ src/com/hexaware/cc/util/DBConnUtil.java
@@ -0,0 +1,16 @@
+package com.hexaware.cc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnUtil {
+    public static Connection getConnection(Properties props) throws SQLException {
+        String url = props.getProperty("url");
+        String user = props.getProperty("user");
+        String password = props.getProperty("password");
+
+        return DriverManager.getConnection(url, user, password);
+    }
+}
